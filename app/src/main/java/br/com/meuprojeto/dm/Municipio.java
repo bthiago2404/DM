fc95cd0c8a808cc2035936971d8cbf4407407404
bthiago2404 <b.thiago2404@gmail.com>
2019-02-07 20:57:11 -0300

    Inicio do projeto (agora é pra valer)!

--- app/src/main/java/br/com/meuprojeto/dm/Municipio.java
+++ app/src/main/java/br/com/meuprojeto/dm/Municipio.java
@@ -12,45 +12,5 @@ public class Municipio {
     // Metodos normais
 
     //Getters e Setters
-    public String getGetCodMunicipio() {
-        return getCodMunicipio;
-    }
 
-    public void setGetCodMunicipio(String getCodMunicipio) {
-        this.getCodMunicipio = getCodMunicipio;
-    }
-
-    public String getMunicipio() {
-        return municipio;
-    }
-
-    public void setMunicipio(String municipio) {
-        this.municipio = municipio;
-    }
-
-    public String getCodUf() {
-        return codUf;
-    }
-
-    public void setCodUf(String codUf) {
-        this.codUf = codUf;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public boolean isExcluido() {
-        return excluido;
-    }
-
-    public void setExcluido(boolean excluido) {
-        this.excluido = excluido;
-    }
-
-    // Acrescentei comentario apenas para validar conhecimentos adiquiridos em repositorios
 }
