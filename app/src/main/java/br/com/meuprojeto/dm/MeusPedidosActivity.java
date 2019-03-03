e9183fc4b444734ba8147aa722c0bc67d66697d6
bthiago2404 <b.thiago2404@gmail.com>
2019-03-03 17:59:11 -0300

    O QUE MUDOU: Alterações de Layout.
    COMO MUDOU (RESUMO): Foi feito alterações para melhorar o Layout da tela inicial e da tela de lista de compras.
    
    AUTOR: bthiago2404

---
+++ app/src/main/java/br/com/meuprojeto/dm/MeusPedidosActivity.java
@@ -0,0 +1,13 @@
+package br.com.meuprojeto.dm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeusPedidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_pedidos);
+    }
+}
