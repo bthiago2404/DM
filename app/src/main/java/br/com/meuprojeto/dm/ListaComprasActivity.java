e34ede0256ea81f80152a0ac7161ddd3e6cf83c7
bthiago2404 <b.thiago2404@gmail.com>
2019-03-06 20:47:12 -0300

    O QUE MUDOU: Tela Departamentos.
    COMO MUDOU (RESUMO): Foi criado a tela Departamentos.
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/ListaComprasActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/ListaComprasActivity.java
@@ -1,7 +1,9 @@
 package br.com.meuprojeto.dm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListaComprasActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class ListaComprasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_compras);
     }
+
+    public void meuCarrinho(View view) {
+        Intent irTelaMeuCarrinho = new Intent(getApplicationContext(), MeuCarrinhoActivity.class);
+        startActivity(irTelaMeuCarrinho);
+    }
+
 }
