5054934465513a6c387b2ef5d767ab78d7d985fb
bthiago2404 <b.thiago2404@gmail.com>
2019-03-10 13:23:26 -0300

    O QUE MUDOU: Detalhes.
    COMO MUDOU (RESUMO): Foi feito algumas interações simples. Nada de grandes mudanças.
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/LojaSelecionadaActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/LojaSelecionadaActivity.java
@@ -1,7 +1,9 @@
 package br.com.meuprojeto.dm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LojaSelecionadaActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class LojaSelecionadaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loja_selecionada);
     }
+
+    public void produto(View view) {
+        Intent irTelaProdutos = new Intent(getApplicationContext(), ProdutoActivity.class);
+        startActivity(irTelaProdutos);
+    }
+
 }
