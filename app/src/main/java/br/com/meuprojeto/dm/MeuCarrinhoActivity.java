1e166932c2f93f68fd41e3d2cb93244db4f2d131
bthiago2404 <b.thiago2404@gmail.com>
2019-03-20 22:30:45 -0300

    O QUE MUDOU: Inicio do Back-End.
    COMO MUDOU (RESUMO): Fiz alterações simples na tela Meu Carrinho e em sua classe.
    O primeiro botão de acrescentar está funcionando. Ainda falta os demais, e falta também a soma dos valores.
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/MeuCarrinhoActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/MeuCarrinhoActivity.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MeuCarrinhoActivity extends AppCompatActivity {
+
+    private Button acrescentar;
+    private Button subtrair;
+    private TextView quantidade;
+    private int i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meu_carrinho);
+
+        acrescentar = findViewById(R.id.btnAcrescentar);
+        subtrair = findViewById(R.id.btnSubtrair);
+        quantidade = findViewById(R.id.txtResultado);
+
     }
 
     // Interação entre a tela MEU CARRINHO e a tela CONFIRMAÇÃO DE PEDIDO
@@ -18,6 +31,18 @@ public class MeuCarrinhoActivity extends AppCompatActivity {
     public void meuCarrinho(View view) {
         Intent irTelaConfirmacaoPedido = new Intent(getApplicationContext(), ConfirmacaoPedidoActivity.class);
         startActivity(irTelaConfirmacaoPedido);
+    }
+
+    public void acrescentarItens(View view) {
+        i = i + 1;
+        String numero2 = String.valueOf(i);
+        quantidade.setText(numero2);
+    }
+
+    public void subtrairItens(View view) {
+        i = i - 1;
+        String numero2 = String.valueOf(i);
+        quantidade.setText(numero2);
     }
 
 }
