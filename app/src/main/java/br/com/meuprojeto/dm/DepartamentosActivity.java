f2c0e830861f414223c59c63cbe3aea6bb7e990e
Thiago <b.thiago2404@gmail.com>
2019-04-19 19:56:20 -0300

    O QUE MUDOU: Tela Departamentos
    COMO MUDOU (RESUMO): Foi iniciado a criação da tela Departamentos. Estou salvando pois vou fazer uma mudança nos pacotes e se houver erro tenho o backup.
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/DepartamentosActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/DepartamentosActivity.java
@@ -2,12 +2,32 @@ package br.com.meuprojeto.dm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class DepartamentosActivity extends AppCompatActivity {
+
+    private RecyclerView rvDepartamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_departamentos);
+
+        rvDepartamentos = findViewById(R.id.rvDepartamentos);
+
+        // Configurando o adapter
+
+
+
+        // Configurando o RyceclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        rvDepartamentos.setLayoutManager(layoutManager);
+        rvDepartamentos.setHasFixedSize(true);
+        //rvDepartamentos.setAdapter(  );
+
     }
+
+
+
 }
