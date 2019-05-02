dacb20353a337038df3783c995fa48ef977646a5
Thiago <b.thiago2404@gmail.com>
2019-05-01 22:12:02 -0300

    O QUE MUDOU: Tela Confirmação Pedido.
    COMO MUDOU (RESUMO): Foi concluido o layout da tela Confirmação de Pedidos. Falta fazer agora o resto da logica (no caso pegar as referencias dos itens comprados na tela de finalizar pedidos e replicar para essa tela de confirmação, fazer também a logica que soma a quantidade de itens e o valor total da feira para exibir no final da tela).
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/model/ConfirmacaoPedidoModel.java
+++ app/src/main/java/br/com/meuprojeto/dm/model/ConfirmacaoPedidoModel.java
@@ -2,4 +2,60 @@ package br.com.meuprojeto.dm.model;
 
 public class ConfirmacaoPedidoModel {
 
+    private String quantidadeItem;
+    private String nomeItem;
+    private String valorItem;
+    private String quantidadeTotalItem;
+    private String valorTotal;
+
+    public ConfirmacaoPedidoModel() {
+
+    }
+
+    public ConfirmacaoPedidoModel(String quantidadeItem, String nomeItem, String valorItem) {
+        this.quantidadeItem = quantidadeItem;
+        this.nomeItem = nomeItem;
+        this.valorItem = valorItem;
+    }
+
+    public String getQuantidadeItem() {
+        return quantidadeItem;
+    }
+
+    public void setQuantidadeItem(String quantidadeItem) {
+        this.quantidadeItem = quantidadeItem;
+    }
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public String getValorItem() {
+        return valorItem;
+    }
+
+    public void setValorItem(String valorItem) {
+        this.valorItem = valorItem;
+    }
+
+    public String getQuantidadeTotalItem() {
+        return quantidadeTotalItem;
+    }
+
+    public void setQuantidadeTotalItem(String quantidadeTotalItem) {
+        this.quantidadeTotalItem = quantidadeTotalItem;
+    }
+
+    public String getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(String valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
 }
