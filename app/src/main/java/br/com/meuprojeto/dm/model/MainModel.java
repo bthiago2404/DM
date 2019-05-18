270c64d5c9fcddf906604ba12b684a6ea7681856
Thiago <b.thiago2404@gmail.com>
2019-05-18 16:28:41 -0300

    O QUE MUDOU: Tela Main.
    COMO MUDOU (RESUMO): Tela Main parcialmente concluida. Agora vou trabalhar no acabamento dela, para ver o que da para melhorar!
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/model/MainModel.java
+++ app/src/main/java/br/com/meuprojeto/dm/model/MainModel.java
@@ -2,4 +2,41 @@ package br.com.meuprojeto.dm.model;
 
 public class MainModel {
 
+    private String loja;
+    private String tempoEntrega;
+    private String valorEntrega;
+
+    public MainModel() {
+    }
+
+    public MainModel(String loja, String tempoEntrega, String valorEntrega) {
+        this.loja = loja;
+        this.tempoEntrega = tempoEntrega;
+        this.valorEntrega = valorEntrega;
+    }
+
+    public String getLoja() {
+        return loja;
+    }
+
+    public void setLoja(String loja) {
+        this.loja = loja;
+    }
+
+    public String getTempoEntrega() {
+        return tempoEntrega;
+    }
+
+    public void setTempoEntrega(String tempoEntrega) {
+        this.tempoEntrega = tempoEntrega;
+    }
+
+    public String getValorEntrega() {
+        return valorEntrega;
+    }
+
+    public void setValorEntrega(String valorEntrega) {
+        this.valorEntrega = valorEntrega;
+    }
+
 }
