ba6b4a815ae11396542f267920282a13406a3d72
Thiago <b.thiago2404@gmail.com>
2019-05-25 22:35:20 -0300

    O QUE MUDOU: Tela Ver Mais.
    COMO MUDOU (RESUMO): Foi feito a tela Ver Mais. Ainda está com um pequeno erro. Quando clica no produto para ir ver mais detalhes do mesmo não vai. Tem que clicar e segurar para poder ir. Mais nas proximas atualizações resolvo isso.
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/activity/VerMaisActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/activity/VerMaisActivity.java
@@ -1,15 +1,134 @@
 package br.com.meuprojeto.dm.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.meuprojeto.dm.R;
+import br.com.meuprojeto.dm.adapter.VerMaisAdapter;
+import br.com.meuprojeto.dm.model.VerMaisModel;
+import br.com.meuprojeto.dm.outros.RecyclerItemClickListener;
 
 public class VerMaisActivity extends AppCompatActivity {
+
+    private RecyclerView rvVerMais;
+    private List<VerMaisModel> verMaisActivity = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_mais);
+
+        // Apontei a variavel rvVerMais para o recyclerview rvVerMais da tela.
+        rvVerMais = findViewById(R.id.rvVerMais);
+
+        // Listagens de Departamentos
+        this.listaVerMais();
+
+        // Configurando o Adapter (o adapter serve para receber os dados, sejam eles de uma
+        // matriz, vetor ou de uma base de dados), apos feito isso o adapter vai tratar os dados
+        // e servir como parametro para ser usado posteriormente no ListView.
+        VerMaisAdapter adapter = new VerMaisAdapter(verMaisActivity);
+
+        // Adicionando o adaptador para a RecyclerView.
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        rvVerMais.setLayoutManager(layoutManager);
+        rvVerMais.setHasFixedSize(true);
+        rvVerMais.setAdapter(adapter);
+
+        // Adicionando cliqe na lista
+        rvVerMais.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        rvVerMais,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+
+                            // Esse metodo é responsavel pelo click unico no item da lista.
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                /*
+                                Intent irTelaDepartamento = new Intent(getApplicationContext(), CategoriasActivity.class);
+                                startActivity(irTelaDepartamento);
+                                */
+                            }
+
+                            // Esse metodo é responsavel pelo click longo no item da lista.
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                /*
+                                DepartamentosModel departamentosModel = departamentosActivity.get(position);
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "" + departamentosModel.getTvdepartamentos(),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                                */
+                            }
+
+                            //
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
+
     }
+
+    //Metodo responsavel por levar o cliente até a tela do produto.
+    //OBS: esse metodo tem que fazer um filtro para levar o cliente para a tela do produto certo.
+    public void irTelaProduto(View view) {
+        Intent irTelaProduto = new Intent(getApplicationContext(), ProdutoActivity.class);
+        startActivity(irTelaProduto);
+    }
+
+    public void irTelaTipoEntrega(View view) {
+        Intent irTelaTipoEntrega = new Intent(getApplicationContext(), TipoEntregaActivity.class);
+        startActivity(irTelaTipoEntrega);
+    }
+
+    public void irTelaMeuCarrinho(View view){
+        Intent irTelaMeuCarrinho = new Intent(getApplicationContext(), MeuCarrinhoActivity.class);
+        startActivity(irTelaMeuCarrinho);
+    }
+
+
+    // Metodo responsavel por gerenciar a lista de departamentos que eu tenho.
+    // esse metodo está trabalhando com uma lista de departamentos engessada. Futuramente quero que
+    // essa lista seja alimentada por uma consulta no banco de dados.
+    public void listaVerMais(){
+
+        //DepartamentosModel listaDepartamentos = new DepartamentosModel("Alimentos Especiais");
+        //this.departamentosActivity.add(listaDepartamentos);
+
+        VerMaisModel listaVerMais = new VerMaisModel("Azeitona Verde Raiola 100g Fatiada", "5.99", "5.99", "Azeitona Verde Raiola 100g Fatiada", "8.99", "8.99");
+        this.verMaisActivity.add(listaVerMais);
+
+        listaVerMais = new VerMaisModel("Atum Gomes Da Costa 170g Solido Natural Light", "1.50", "5.99", "Beb Lactea Nestle 200ml Nescau Light", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+        listaVerMais = new VerMaisModel("Bisc Capricche 120g Waffer Laranja", "3.99", "5.99", "Agua Mineral Indaia 200ml Copo", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+        listaVerMais = new VerMaisModel("Alface Crespa Und", "3.20", "5.99", "Alvejante Brilux Lv5l Pg4,5l Mult", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+        listaVerMais = new VerMaisModel("Amendoim Pippo S 30g S-Pele Torrado", "0.80", "0.80", "Ap Barbear Bic Comf 3 Black Night C-1", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+        listaVerMais = new VerMaisModel("Arroz Safra De Ouro Da Terra 1kg", "5.20", "5.20", "Aveia Yoki 500g Sachet Em Flocos Finos", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+        listaVerMais = new VerMaisModel("Azeitona Verde Raiola 100g Fatiada", "5.99", "5.99", "Feijão Preto tip 1", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+        listaVerMais = new VerMaisModel("Barra Cereal Hersheys 66g Cookies-Choc", "10.00", "10.00", "Batata Frita 50g Scrusch Ondulada", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+        listaVerMais = new VerMaisModel("Bebida Lactea Cariri 900g Zero Morango", "5.99", "5.99", "Bisc Amant Renata 330g Coco", "8.99", "8.99");
+        verMaisActivity.add(listaVerMais);
+
+    }
+
 }
---
+++ app/src/main/java/br/com/meuprojeto/dm/adapter/VerMaisAdapter.java
@@ -0,0 +1,82 @@
+package br.com.meuprojeto.dm.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.meuprojeto.dm.R;
+import br.com.meuprojeto.dm.model.VerMaisModel;
+
+public class VerMaisAdapter extends RecyclerView.Adapter<VerMaisAdapter.MyViewHolder> {
+
+    // Criei um atributo para ser usado no decorrer do codigo abaixo.
+    private List<VerMaisModel> verMaisAdapter;
+
+    // Metodo construtor da classe VerMaisAdapter.
+    public VerMaisAdapter(List<VerMaisModel> lista) {
+        this.verMaisAdapter = lista;
+    }
+
+    // Metodo abaixo apenas cria a visualização dos itens. Pelo que eu entendi ate o momento,
+    // o metodo abaixo traz o layout em xml e converte para um objeto para poder ser usado
+    // como referencia para ser instanciado posteriormente.
+    @NonNull
+    @Override
+    public VerMaisAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemLista = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.adapter_ver_mais, viewGroup, false);
+
+        return new VerMaisAdapter.MyViewHolder(itemLista);
+    }
+
+    // O metodo abaixo é responsavel por fazer a exibição dos items no RecycleView.
+    @Override
+    public void onBindViewHolder(@NonNull VerMaisAdapter.MyViewHolder myViewHolder, int i) {
+
+        VerMaisModel listaVerMais = verMaisAdapter.get(i);
+
+        myViewHolder.tvNomeProduto1.setText(listaVerMais.getTvNomeProduto1());
+        myViewHolder.tvValorProduto1.setText(listaVerMais.getTvValorProduto1());
+        myViewHolder.tvValor1.setText(listaVerMais.getTvValor1());
+
+        myViewHolder.tvNomeProduto2.setText(listaVerMais.getTvNomeProduto2());
+        myViewHolder.tvValorProduto2.setText(listaVerMais.getTvValorProduto2());
+        myViewHolder.tvValor2.setText(listaVerMais.getTvValor2());
+
+    }
+
+    // Esse metodo é responsavel por retornar a quantidade de itens que vão ser exibidos.
+    @Override
+    public int getItemCount() { return verMaisAdapter.size(); }
+
+    // Inner class necessario para o funcionamento perfeito do adapter.
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvNomeProduto1;
+        TextView tvValorProduto1;
+        TextView tvValor1;
+
+        TextView tvNomeProduto2;
+        TextView tvValorProduto2;
+        TextView tvValor2;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvNomeProduto1 = itemView.findViewById(R.id.tvNomeProduto1);
+            tvValorProduto1 = itemView.findViewById(R.id.tvValorProduto1);
+            tvValor1 = itemView.findViewById(R.id.tvValor1);
+
+            tvNomeProduto2 = itemView.findViewById(R.id.tvNomeProduto2);
+            tvValorProduto2 = itemView.findViewById(R.id.tvValorProduto2);
+            tvValor2 = itemView.findViewById(R.id.tvValor2);
+
+        }
+    }
+
+}
