a62238e21da26e503d7e05174e71f6e7996f2126
Thiago <b.thiago2404@gmail.com>
2019-06-16 22:50:51 -0300

    O QUE MUDOU: Tela MEus Dados.
    COMO MUDOU (RESUMO): Validando os campos da tela Meus Dados.
    (Obs: Ainda está com erro no ultimo campo Confirme sua Nova Senha. Deixei comentado para resolver depois).
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/activity/MeusDadosActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/activity/MeusDadosActivity.java
@@ -61,11 +61,33 @@ public class MeusDadosActivity extends AppCompatActivity {
     // Metodo responsabel por salvar os dados do cliente no banco. (Em Construção).
     public void salvar(View view){
 
-        verificaCheck();
-        Toast.makeText(getApplicationContext(),"Dados alterados com sucesso!", Toast.LENGTH_LONG).show();
-        Intent irTelaInicial = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(irTelaInicial);
-        MeusDadosActivity.this.finish();
+        if(etNome.getText().toString().length() == 0){
+            etNome.setError(getString(R.string.txt_erro_nome));
+        } else if(etNumero.getText().toString().length() == 0){
+            etNumero.setError(getString(R.string.txt_erro_telefone));
+        } else if(etEmail.getText().toString().length() == 0){
+            etEmail.setError(getString(R.string.txt_erro_email2));
+        } else if(etRua.getText().toString().length() == 0){
+            etRua.setError(getString(R.string.txt_erro_rua));
+        } else if(etBairro.getText().toString().length() == 0){
+            etBairro.setError(getString(R.string.txt_erro_bairro));
+        } else if(etNum.getText().toString().length() == 0){
+            etNum.setError(getString(R.string.txt_erro_numero));
+        } else if(etSenha.getText().toString().length() == 0){
+            etSenha.setError(getString(R.string.txt_erro_senha_atual));
+        } else if(etNovaSenha.getText().toString().length() == 0) {
+            etNovaSenha.setError(getString(R.string.txt_erro_nova_senha));
+        /*
+        } else if(etConfirmeNovaSenha.getText().toString().length() == 0){
+            etConfirmeNovaSenha.setError(getString(R.string.txt_erro_confirme_nova_senha));
+            */
+        } else {
+            verificaCheck();
+            Toast.makeText(getApplicationContext(),"Dados alterados com sucesso!", Toast.LENGTH_LONG).show();
+            Intent irTelaInicial = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(irTelaInicial);
+            MeusDadosActivity.this.finish();
+        }
 
     }
 
