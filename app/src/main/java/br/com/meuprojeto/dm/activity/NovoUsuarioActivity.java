8c57f57a03fe498999e69428792787581d0ae37f
Thiago <b.thiago2404@gmail.com>
2019-06-26 19:58:31 -0300

    O QUE MUDOU: Detalhes.
    COMO MUDOU (RESUMO): Foi feito melhorias de layout e espaçamento entre botões. Nada de muito importante.
     Falta apenas:
              * Na tela da loja selecionada tentar colocar os produtos em destaques em uma grade de 2 colunas e no maximo 3 linhas.
    
    AUTOR: bthiago2404

--- app/src/main/java/br/com/meuprojeto/dm/activity/NovoUsuarioActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/activity/NovoUsuarioActivity.java
@@ -40,17 +40,17 @@ public class NovoUsuarioActivity extends AppCompatActivity {
     }
 
     // Metodo responsavel por chegar as opções de melhor horario para entrega e inserir no banco.
-    public void verificaCheck(){
+    public void verificaCheck() {
 
-        if(cbManha.isChecked()){
+        if (cbManha.isChecked()) {
             // Trecho do codigo onde será inserido a opção de entrega pela manha no banco de dados.
             // Obs: Aqui não devera conter um INSERT no banco e sim um UPDATE!
         }
-        if(cbTarde.isChecked()){
+        if (cbTarde.isChecked()) {
             // Trecho do codigo onde será inserido a opção de entrega pela tarde no banco de dados.
             // Obs: Aqui não devera conter um INSERT no banco e sim um UPDATE!
         }
-        if(cbNoite.isChecked()){
+        if (cbNoite.isChecked()) {
             // Trecho do codigo onde será inserido a opção de entrega pela noite no banco de dados.
             // Obs: Aqui não devera conter um INSERT no banco e sim um UPDATE!
         }
@@ -58,15 +58,32 @@ public class NovoUsuarioActivity extends AppCompatActivity {
     }
 
     // Metodo responsabel por salvar os dados do cliente no banco. (Em Construção).
-    public void salvar(View view){
+    public void salvar(View view) {
 
-        verificaCheck();
-        Toast.makeText(getApplicationContext(),"Cadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
-        Intent irTelaInicial = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(irTelaInicial);
-        irTelaInicial.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Limpa a pinha de Activity's
-        NovoUsuarioActivity.this.finish(); // Finaliza a activity atual e não permite voltar para ela.
+        if (etNome.getText().toString().length() == 0) {
+            etNome.setError(getString(R.string.txt_erro_nome));
+        } else if (etNumero.getText().toString().length() == 0) {
+            etNumero.setError(getString(R.string.txt_erro_telefone));
+        } else if (etEmail.getText().toString().length() == 0) {
+            etEmail.setError(getString(R.string.txt_erro_email2));
+        } else if (etRua.getText().toString().length() == 0) {
+            etRua.setError(getString(R.string.txt_erro_rua));
+        } else if (etBairro.getText().toString().length() == 0) {
+            etBairro.setError(getString(R.string.txt_erro_bairro));
+        } else if (etNum.getText().toString().length() == 0) {
+            etNum.setError(getString(R.string.txt_erro_numero));
+        } else if (etSenha.getText().toString().length() == 0) {
+            etSenha.setError(getString(R.string.txt_erro_senha_atual));
+        } else if(etConfirmeSenha.getText().toString().length() == 0) {
+            etConfirmeSenha.setError(getString(R.string.txt_erro_senha));
+        } else {
+                verificaCheck();
+                Toast.makeText(getApplicationContext(), "Cadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
+                Intent irTelaInicial = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(irTelaInicial);
+                irTelaInicial.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Limpa a pinha de Activity's
+                NovoUsuarioActivity.this.finish(); // Finaliza a activity atual e não permite voltar para ela.
 
+        }
     }
-
 }
