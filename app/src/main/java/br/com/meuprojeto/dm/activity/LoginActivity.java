0a9a8687145b6ebb28e2dd9de93bbd5409a0e217
saulo <dj_saulod2@hotmail.com>
2019-07-10 19:31:16 -0300

    O QUE MUDOU: Login e Cadastro.
    COMO MUDOU (RESUMO): Foi feito os metodos e parametros para o aplicativo poder fazer login e cadastro.
    (obs: Ainda apresenta erro).
    
    AUTOR: Saulo

--- app/src/main/java/br/com/meuprojeto/dm/activity/LoginActivity.java
+++ app/src/main/java/br/com/meuprojeto/dm/activity/LoginActivity.java
@@ -1,22 +1,32 @@
 package br.com.meuprojeto.dm.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import br.com.meuprojeto.dm.R;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
     // Declaração das variaveis que vao ser usadas.
-    private EditText login;
-    private EditText senha;
+    private EditText etLogin;
+    private EditText etSenha;
     private Switch tipoLogin;
-    private Button entrar;
+    private Button btnEntrar;
+
+    //aqui onde vai a url da API
+    final  String url_Login = "https://deliverymercado.000webhostapp.com/conexaoLogin.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +34,81 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         // Apontando as variaveis para seus respctivos Views da tela login.
-        login = findViewById(R.id.etLogin);
-        senha = findViewById(R.id.etSenha);
+        etLogin = findViewById(R.id.etLogin);
+        etSenha = findViewById(R.id.etSenha);
         tipoLogin = findViewById(R.id.swTipoLogin);
-        entrar = findViewById(R.id.btnEntrar);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                logar();
+                String Email = etLogin.getText().toString();
+                String senha = etSenha.getText().toString();
+
+                new LoginUser().execute(Email, senha);
+
+            }
+        });
+
 
     }
 
+    public  class LoginUser extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String Email = strings[0];
+            String senha = strings[1];
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("EMAIL", Email)
+                    .add("SENHA", senha)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url_Login)
+                    .post(formBody)
+                    .build();
+
+            Response response = null;
+            try{
+                response = okHttpClient.newCall(request).execute();
+                if(response.isSuccessful()){
+                    String result = response.body().string();
+                    if(result.equalsIgnoreCase("login")){
+                        Intent i = new Intent(LoginActivity.this,
+                                MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        showToast("E-mail ou senha incompatíveis!");
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    public void showToast(final String Text){
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LoginActivity.this,
+                        Text, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
     // Metodo que será responsável pelo login no app.
-    public void logar(View view){
+    public void logar(){
 
         if(tipoLogin.isChecked()){
 
@@ -46,10 +122,10 @@ public class LoginActivity extends AppCompatActivity {
 
         } else {
 
-            if(login.getText().toString().length() == 0){
-                login.setError(getString(R.string.txt_erro_email));
-            } else if(senha.getText().toString().length() == 0){
-                senha.setError(getString(R.string.txt_erro_senha));
+            if(etLogin.getText().toString().length() == 0){
+                etLogin.setError(getString(R.string.txt_erro_email));
+            } else if(etSenha.getText().toString().length() == 0){
+                etSenha.setError(getString(R.string.txt_erro_senha));
             } else {
                 Intent irTelaInicial = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(irTelaInicial);
